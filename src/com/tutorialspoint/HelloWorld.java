1e70cc487ec019d58ef7838883917f8e628a6bbb
ADolodarenko <adolodar@gmail.com>
2019-11-16 01:37:29 +0300

    Bean init and destroy callbacks

--- src/com/tutorialspoint/HelloWorld.java
+++ src/com/tutorialspoint/HelloWorld.java
@@ -10,4 +10,12 @@ public class HelloWorld {
 	public void setMessage(String message) {
 		this.message = message;
 	}
+	
+	public void init() {
+		System.out.println("Bean is going through init.");
+	}
+	
+	public void destroy() {
+		System.out.println("Bean will be destroyed now.");
+	}
 }
--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -2,10 +2,18 @@ package com.tutorialspoint;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
 	public static void main(String[] args) {
+		//scopeTest();
+		
+		initAndDestroyTest();
+	}
+	
+	private static void scopeTest() {
 		BeanFactory factory = Utils.getBeanFactory();
 		
 		HelloWorld objA = (HelloWorld) factory.getBean("helloWorld");
@@ -14,5 +22,14 @@ public class MainApp {
 		
 		HelloWorld objB = (HelloWorld) factory.getBean("helloWorld");
 		objB.getMessage();
+	}
+	
+	private static void initAndDestroyTest() {
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		
+		context.registerShutdownHook();
 	}
 }
